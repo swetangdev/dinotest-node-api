21e9cdefa16b1bea2c0ea37f7a5b11a66ea8cdab
Swetang Dev <iamdevgeek@gmail.com>
2022-02-03 01:18:13 +0000

    index code updated for port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const { API_PORT } = process.env.PORT;
-const port = API_PORT || 4000;
+// const API_PORT = process.env.PORT;
+const port = process.env.PORT;
 const app = require('./app');
 
 // server created using express and running at port 3000 (mentioned in .env file at root location)
